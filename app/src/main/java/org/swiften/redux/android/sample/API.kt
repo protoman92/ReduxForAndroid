a32970378004156c075b9cba934d8a27efbecac5
Hai Pham <swiften.svc@gmail.com>
2019-01-27 03:34:55 +0800

    feat(data): add data types, API and Repository

---
+++ app/src/main/java/org/swiften/redux/android/sample/API.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.android.sample
+
+import android.net.Uri
+import java.net.URL
+
+/** Created by haipham on 27/1/19 */
+interface ISearchAPI<Result> {
+  fun searchMusicStore(query: String): Result
+}
+
+class API : ISearchAPI<String> {
+  override fun searchMusicStore(query: String): String {
+    val url = Uri.Builder()
+      .scheme("https")
+      .authority("itunes.apple.com")
+      .appendPath("search")
+      .appendQueryParameter("term", query)
+      .appendQueryParameter("limit", "5")
+      .appendQueryParameter("media", "music")
+      .build()
+      .toString()
+
+    return URL(url).readText()
+  }
+}
---
+++ app/src/main/java/org/swiften/redux/android/sample/Data.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.android.sample
+
+import java.io.Serializable
+
+/** Created by haipham on 27/1/19 */
+data class MusicTrack(
+  val artistName: String,
+  val currency: String,
+  val previewUrl: String,
+  val trackName: String,
+  val trackPrice: Double,
+  val trackTimeMillis: Int
+) : Serializable
+
+data class MusicResult(
+  val resultCount: Int,
+  val results: List<MusicTrack>
+) : Serializable
